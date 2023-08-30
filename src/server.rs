69c433814dbeda81268f59e331a5b4e2be7ca7dc
KavanPrice <kavanprice@gmail.com>
2023-08-30 22:46:54 +0100

    Accept incoming http connections
    - Accepts incoming connections
    - Prints the TCP stream to stdio
    - Error handling for connection and stream reading

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
 
 pub struct Server {
@@ -13,5 +14,19 @@ impl Server {
     pub fn run(self) {
         let listener: TcpListener = TcpListener::bind(&self.addr).unwrap();
         println!("Listening on {}", self.addr);
+
+        // accept incoming http connections
+        loop {
+            match listener.accept() {
+                Ok((mut stream,addr)) => {
+                    let mut buffer: [u8; 1024] = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => println!("Recieved on address {}: {}", addr, String::from_utf8_lossy(&buffer)),
+                        Err(e) => println!("Error on reading stream from address {}: {}", addr, e)
+                    }
+                },
+                Err(e) => println!("Error on trying to connect: {}", e)
+            }
+        }
     }
 }
